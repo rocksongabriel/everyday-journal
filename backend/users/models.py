03ab3f3beff1f2e1acc3ed8febebea005a8ca885
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-22 04:40:17 +0000

    Add Custom User Model and Manager

---
+++ backend/users/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+
+# Create custom user manager
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+        email = self.normalize_email(email)
+        
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password)
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+
+        return user
+
+
+# Create custom user
+class User(AbstractBaseUser):
+    email = models.EmailField(_("Email"), max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
