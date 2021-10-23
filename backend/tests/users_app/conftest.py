1a8bce915569e44d87fa1a644b5dd8b830ed2192
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-23 10:51:53 +0000

    Add extra fixtures

--- backend/tests/users_app/conftest.py
+++ backend/tests/users_app/conftest.py
@@ -1,5 +1,29 @@
 from pytest_factoryboy import register
+import pytest
 
 from .factories import UserFactory
 
 register(UserFactory)
+
+
+# Fixtures
+
+@pytest.fixture
+def validated_user_data():
+    return {
+        "email": "testmail@gmail.com",
+        "password": "testpass1234"
+    }
+
+@pytest.fixture
+def invalid_user_data_no_password():
+    return {
+        "email": "testmail@gmail.com"
+    }
+
+@pytest.fixture
+def invalid_user_data_no_email():
+    return {
+        "password": "testpass1234"
+    }
+
