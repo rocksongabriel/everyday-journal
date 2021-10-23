b42bf033d936876fd460c2bb070de18c650222bd
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-23 09:01:29 +0000

    Add UserRegisterSerializer serializer for serializing and deserializing a model

---
+++ backend/users/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["email", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
