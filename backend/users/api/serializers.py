2d4fb3940e7811c674243fbb4f7a2fab311b03b1
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-26 21:17:50 +0000

    Add user retrieve serializer

--- backend/users/api/serializers.py
+++ backend/users/api/serializers.py
@@ -13,3 +13,9 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["email", "full_name", "bio", "age", "avatar"]
