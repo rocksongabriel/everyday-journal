1cb51eba5544082e5725b1011c8e944a3688c925
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-26 21:17:33 +0000

    Add View for retrieving one user and it's url conf

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegisterAPIView
+from .views import UserRegisterAPIView, UserRetrieveAPIView
 
 
 urlpatterns = [
-    path('users/account/register/', UserRegisterAPIView.as_view(), name="ep-register-user")
+    path('users/account/register/', UserRegisterAPIView.as_view(), name="ep-register-user"),
+    path('users/account/retrieve/<uuid:user_id>/', UserRetrieveAPIView.as_view(), name="ep-retrieve-user")
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.api.serializers import UserRegisterSerializer
+from users.api.serializers import UserProfileSerializer, UserRegisterSerializer
+
+User = get_user_model()
 
 
 class UserRegisterAPIView(APIView):
@@ -13,3 +16,11 @@ class UserRegisterAPIView(APIView):
             user = serializer.save()
             return Response({"email": user.email}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserRetrieveAPIView(APIView):
+
+    def get(self, request, user_id):
+        user = User.objects.get(user_id=user_id)
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
