741abc9258d576b1d54506e921a03e4b996858ef
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-30 04:42:52 +0000

    Add tests for the Update and Retrieve API views of the user model

--- backend/tests/users_app/test_serializers.py
+++ backend/tests/users_app/test_serializers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from users.api.serializers import UserRegisterSerializer
+from users.api.serializers import User, UserProfileSerializer, UserRegisterSerializer, UserUpdateSerializer
 
 pytestmark = pytest.mark.django_db
 
@@ -9,16 +9,50 @@ pytestmark = pytest.mark.django_db
 
 class TestUserRegisterSerializer:
 
-    def test_user_serializer__serialization(self, user_factory):
+    def test_serialization(self, user_factory):
         user = user_factory()
         serializer = UserRegisterSerializer(user)
 
         assert serializer.data
 
-    def test_user_serializer__deserialization(self,validated_user_data):
+    def test_deserialization(self,validated_user_data):
         serializer = UserRegisterSerializer(data=validated_user_data)
 
         if serializer.is_valid():
             user = serializer.save()
         assert user.email == serializer.validated_data["email"]
     
+
+class TestUserUpdateSerializer:
+
+    def test_serilization(self, user_factory):
+        user = user_factory()
+        serializer = UserUpdateSerializer(user)
+
+        assert serializer.data
+
+    def test_deserialization(self, user_factory, validated_data):
+        user = user_factory()
+        serializer = UserUpdateSerializer(user, data=validated_data)
+
+        if serializer.is_valid():
+            serializer.save()
+        
+        assert serializer.data.get("full_name")
+        assert serializer.data["age"]
+        assert serializer.data["bio"]
+
+
+class TestUserRetrieveSerializer:
+
+    def test_serialization(self):
+        user = User.objects.create(
+            email="testemail.com",
+            password="testpass1234",
+            full_name="Test User",
+            bio="Let's do this",
+            age=29,
+        )
+
+        serializer = UserProfileSerializer(user)
+        assert serializer.data
