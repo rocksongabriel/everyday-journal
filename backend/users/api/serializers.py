6ecba178423fe16b34ab1a483983ebb7c0982845
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-30 10:08:58 +0000

    Modify the password validation in the User Registrtion serializer

--- backend/users/api/serializers.py
+++ backend/users/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth import password_validation
+import django.contrib.auth.password_validation as validators
+from django.core import exceptions
 
 User = get_user_model()
 
@@ -11,9 +12,23 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ["email", "password"]
 
-    def validate_password(self, password):
-        password_validation.validate_password(password)
-        return password
+    def validate(self, data):
+        user = User(**data)
+        password = data["password"]
+        
+        errors = dict() 
+        try:
+            # validate the password and catch the exception
+            validators.validate_password(password=password, user=user)
+
+         # the exception raised here is different than serializers.ValidationError
+        except exceptions.ValidationError as e:
+             errors['password'] = list(e.messages)
+
+        if errors:
+             raise serializers.ValidationError(errors)
+
+        return super(UserRegisterSerializer, self).validate(data)
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
